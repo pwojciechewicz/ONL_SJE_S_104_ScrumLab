1d1b94a96540bd58ba808f19a1ba8b69e41284dc
Michalina Pudlo <pudlomichalina203@gmail.com>
2022-10-10 17:19:34 +0200

    0RecipeDao

--- src/main/java/pl/coderslab/utils/DbUtil.java
+++ src/main/java/pl/coderslab/utils/DbUtil.java
@@ -5,13 +5,19 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DbUtil {
     private static DataSource dataSource;
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/scrumlab?useSSL=false&characterEncoding=utf8";
+    private static final String DB_USER = "root";
+    private static final String DB_PASS = "coderslab";
+
 
     public static Connection getConnection() throws SQLException {
-        return getInstance().getConnection();
+        return DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
+//        return getInstance().getConnection();
     }
 
     private static DataSource getInstance() {
