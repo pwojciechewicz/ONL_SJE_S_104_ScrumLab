9628b34a4066a3b720b74687def84b87b50efb9a
Cyluna <lucynalis@gmail.com>
2022-10-11 00:13:16 +0200

    #0

--- src/main/java/pl/coderslab/web/HomeServlet.java
+++ src/main/java/pl/coderslab/web/HomeServlet.java
@@ -1,7 +1,9 @@
 package pl.coderslab.web;
 
-import pl.coderslab.dao.BookDao;
-import pl.coderslab.model.Book;
+//import pl.coderslab.dao.BookDao;
+import pl.coderslab.dao.DayNameDao;
+//import pl.coderslab.model.Book;
+import pl.coderslab.model.DayName;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -18,9 +20,9 @@ import java.util.List;
 public class HomeServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        BookDao bookDao = new BookDao();
-        List<Book> books = bookDao.findAll();
-        System.out.println(books);
+        DayNameDao dayNameDao = new DayNameDao();
+        List<DayName> dayNames = dayNameDao.findAll();
+        System.out.println(dayNames);
 
         getServletContext().getRequestDispatcher("/home.jsp").forward(request, response);
     }
